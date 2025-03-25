f29f008044df29e0f24ad480f2c5a1bb462ee905
Adli <adli.putra@nawadata.com>
2025-03-26 05:16:21 +0700

    Initial Commit

---
+++ user-service/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace user_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new List<object>
+            {
+                new { Id = 1, Name = "John Doe" },
+                new { Id = 2, Name = "Jane Doe" }
+            };
+
+            return Ok(users);
+        }
+    }
+}
